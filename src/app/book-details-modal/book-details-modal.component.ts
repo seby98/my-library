d41c5efc14ae2332b95e4c2b3c5385aaa7973370
Seby Maesano <seby.maesano@yahoo.it>
2019-04-05 00:13:45 +0200

    modal detail style complete and fixed data rendering

--- src/app/book-details-modal/book-details-modal.component.ts
+++ src/app/book-details-modal/book-details-modal.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { RemoteBookService } from '../services/remote-book/remote-book.service';
 import { GoogleBookResponse } from '../interfaces/google-book-response';
-import { VolumeInfo, VolumeInfo_ImageLinks, VolumeInfo_IndustryIdentifier } from '../interfaces/google-volume-info';
+import { TemplateBookDetails } from '../interfaces/template-book-details';
+import { map } from 'rxjs/operators';
+import { VolumeInfo_IndustryIdentifier } from '../interfaces/google-volume-info';
 
 @Component({
   selector: 'app-book-details-modal',
@@ -11,7 +13,7 @@ import { VolumeInfo, VolumeInfo_ImageLinks, VolumeInfo_IndustryIdentifier } from
 })
 export class BookDetailsModalComponent implements OnInit {
 
-  public bookDetails: VolumeInfo;
+  public templateBookDetails: TemplateBookDetails;
   public retriveRemoteInfoComplete: boolean;
 
   constructor(
@@ -20,20 +22,41 @@ export class BookDetailsModalComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data
   ) {
     this.retriveRemoteInfoComplete = false;
-
-    this.bookDetails = {} as VolumeInfo;
-    this.bookDetails.imageLinks = {} as VolumeInfo_ImageLinks;
-    this.bookDetails.authors = [];
-    this.bookDetails.categories = [] as string[];
-    this.bookDetails.industryIdentifiers = [] as VolumeInfo_IndustryIdentifier[];
+    this.templateBookDetails = {
+      title: "",
+      thumbnail: "",
+      description: "",
+      authors: "",
+      categories: "",
+      isbn: "",
+      publisher: "",
+      publishedDate: "",
+      pageCount: 0
+    };
   }
 
   ngOnInit() {
-    this.remoteBookService.getBook(this.data.bookID).subscribe(
-      (res: GoogleBookResponse) => {
-        this.bookDetails = res.volumeInfo;
+    this.remoteBookService.getBook(this.data.bookID)
+    .pipe(
+      map((res: GoogleBookResponse) => {
+        const _templateBookDetails: TemplateBookDetails = {
+          title: res.volumeInfo.title,
+          thumbnail: res.volumeInfo.imageLinks.thumbnail,
+          description: res.volumeInfo.description,
+          authors: res.volumeInfo.authors ? this.getStringToArray(res.volumeInfo.authors) : "",
+          categories: res.volumeInfo.categories ? this.getStringToArray(res.volumeInfo.categories) : "",
+          isbn: res.volumeInfo.industryIdentifiers ? this.getIsbn13(res.volumeInfo.industryIdentifiers) : "",
+          publisher: res.volumeInfo.publisher,
+          publishedDate: res.volumeInfo.publishedDate,
+          pageCount: res.volumeInfo.pageCount
+        }
+        return _templateBookDetails;
+      })
+    )
+    .subscribe(
+      (__templateBookDetails: TemplateBookDetails) => {
+        this.templateBookDetails = __templateBookDetails;
         this.retriveRemoteInfoComplete = true;
-        console.log(res);
       },
       error => console.log(error)
     );
@@ -41,5 +64,20 @@ export class BookDetailsModalComponent implements OnInit {
 
   closeModal() {
     this.modalRef.close();
+  }
+
+  getStringToArray(_array: string[]): string {
+    let _string: string = "";
+    _array.forEach((strItem: string) => {
+      _string = _string + strItem + " - ";
+    });
+    _string = _string.substr(0, _string.length - 3);
+    return _string;
+  }
+
+  getIsbn13(isbnArray: VolumeInfo_IndustryIdentifier[]): string {
+    let isbn: string = "";
+    isbnArray.forEach((isbnItem: VolumeInfo_IndustryIdentifier) => isbnItem.type === "ISBN_13" ? isbn = isbnItem.identifier : "");
+    return isbn;
   }
 }
---
+++ src/app/interfaces/template-book-details.ts
@@ -0,0 +1,11 @@
+export interface TemplateBookDetails {
+    title: string
+    thumbnail: string
+    description: string
+    authors: string
+    categories: string
+    isbn: string
+    publisher: string
+    pageCount: number
+    publishedDate: string
+}
